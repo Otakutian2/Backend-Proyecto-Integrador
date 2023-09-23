0a298caf6a0ccbadb59454d282173e331321a338
Jorge Anthony <jorgito09.82chavez@gmail.com>
2023-09-22 22:57:40 -0500

    Subiendo models,interface,schemas (cash,establishment,paymentMethod)

---
+++ Interfaces/IEstablishment.cs
@@ -0,0 +1,14 @@
+﻿using project_backend.Models;
+
+namespace project_backend.Interfaces
+{
+    public interface IEstablishment
+    {
+        public Task<List<Establishment>> GetAll();
+        public Task<Establishment> GetById(int id);
+        public Task<Establishment> GetFirstOrDefault();
+        public Task<bool> CreateEstablishment(Establishment establishment);
+        public Task<bool> UpdateEstablishment(Establishment establishment);
+        public Task<bool> DeleteEstablishment(Establishment establishment);
+    }
+}
---
+++ Models/Cash.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace project_backend.Models
+{
+    public class Cash
+    {
+        public int Id { get; set; }
+
+        [Column("establishment_id")]
+        public int EstablishmentId { get; set; }
+        public Establishment Establishment { get; set; }
+
+      /*  public List<Receipt> ReceiptCollection { get; } = new List<Receipt>();*/
+    }
+}
