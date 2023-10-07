c4bda21f61f0ef0d8d38fceeed9b7a1dc850f47f
Jorge Anthony <jorgito09.82chavez@gmail.com>
2023-10-06 21:43:13 -0500

    enviando (Cash,PaymenMethod)

---
+++ Services/CashService.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using project_backend.Data;
+using project_backend.Interfaces;
+using project_backend.Models;
+using System.Linq.Expressions;
+
+namespace project_backend.Services
+{
+    public class CashService : ICash
+    {
+        private readonly CommandContext _context;
+
+        public CashService(CommandContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateCash(Cash cash)
+        {
+            bool result = false;
+
+            try
+            {
+                _context.Cash.Add(cash);
+                await _context.SaveChangesAsync();
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return result;
+        }
+
+        public async Task<bool> DeleteCash(Cash cash)
+        {
+            bool result = false;
+
+            try
+            {
+                _context.Cash.Remove(cash);
+                await _context.SaveChangesAsync();
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return result;
+        }
+
+        public async Task<List<Cash>> GetAll()
+        {
+            List<Cash> listCash = await _context.Cash.Include(c => c.Establishment).ToListAsync();
+
+            return listCash;
+        }
+
+        public async Task<Cash> GetById(int id)
+        {
+            var cash = await _context.Cash.Include(c => c.Establishment)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return cash;
+        }
+
+        public async Task<bool> UpdateCash(Cash cash)
+        {
+            bool result = false;
+
+            try
+            {
+                _context.Entry(cash).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return result;
+        }
+
+        public async Task<int> Count(Expression<Func<Cash, bool>> predicate = null)
+        {
+            return await (predicate != null ? _context.Cash.CountAsync(predicate) : _context.Cash.CountAsync());
+        }
+    }
+}
